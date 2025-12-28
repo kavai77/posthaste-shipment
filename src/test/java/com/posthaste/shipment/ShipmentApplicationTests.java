eb9d7a40a965aaa29fe2cecf3f5dc1f15d8e559c
Csaba Kavai <csaba.kavai.osv@fedex.com>
2025-12-28 22:34:48 +0100

    integrating with firestore

--- src/test/java/com/posthaste/shipment/ShipmentApplicationTests.java
+++ src/test/java/com/posthaste/shipment/ShipmentApplicationTests.java
@@ -1,10 +1,15 @@
 package com.posthaste.shipment;
 
+import com.google.firebase.database.FirebaseDatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ShipmentApplicationTests {
+
+	@MockitoBean
+	FirebaseDatabase firebaseDatabase;
 
 	@Test
 	void contextLoads() {
